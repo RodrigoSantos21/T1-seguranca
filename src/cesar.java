ba3d3f01bd4b12fb9a1085affdd46a6019bb7c42
Rodrigo Santos <rodrigoaugusto2103@gmail.com>
2024-10-16 21:24:51 -0300

    Cifra de cesar ajustada e vernam mostrando chave e texto cifrado e descifrado

--- src/cesar.java
+++ src/cesar.java
@@ -1,11 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class cesar {
     public HashMap<Character, Integer> charPosicao = new HashMap<Character, Integer>();
     public HashMap<Integer, Character> posicaoChar = new HashMap<Integer, Character>();
+    String textoDescifrado = "";
+    String textoCifrado = "";
 
     public void main(String[] args) {
         this.montaHashs();
@@ -22,7 +25,8 @@ public class cesar {
 
         }
         int k = 45;
-        this.decifrarCesar(texto.toString(), k);
+        this.cesar(texto.toString(), k, 0);
+        this.cesar(this.textoDescifrado, 62-k, 1);
     }
     public void montaHashs(){
         char [] alfabeto = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
@@ -30,14 +34,23 @@ public class cesar {
             charPosicao.put(alfabeto[i], i);
             posicaoChar.put(i, alfabeto[i]);
         }
+        System.out.println(charPosicao.get('P') + " " + charPosicao.get('g'));
     }
-    public void decifrarCesar(String texto, int k){
+
+    public void cesar(String texto, int k, int cifra){
         char[] decifrada = texto.toCharArray();
-        for(int i = 0; i < decifrada.length; i++){
-            if(charPosicao.get(decifrada[i]) != null){
-                decifrada[i] = posicaoChar.get((charPosicao.get(decifrada[i]) + k)%62);
+        for(int i = 0; i < decifrada.length; i++) {
+            if (charPosicao.get(decifrada[i]) != null) {
+                    decifrada[i] = posicaoChar.get((Math.abs(charPosicao.get(decifrada[i]) + k)) % 62);
             }
         }
-        System.out.println(decifrada);
+        if(cifra == 0){
+            this.textoDescifrado = new String(decifrada);
+            System.out.println("Texto descifrado com cesar com k igual a " + k + ": " + this.textoDescifrado);
+        }
+        else{
+            this.textoCifrado = new String(decifrada);
+            System.out.println("Texto cifrado com cesar com k igual a " + k + ": " + this.textoCifrado);
+        }
     }
 }
--- src/vernam.java
+++ src/vernam.java
@@ -6,8 +6,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class vernam {
-    public HashMap<Character, Integer> charPosicao = new HashMap<Character, Integer>();
-    public HashMap<Integer, Character> posicaoChar = new HashMap<Integer, Character>();
     char [] alfabeto = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
     String chave = "";
     String textoCifrado = "";
@@ -23,7 +21,6 @@ public class vernam {
         }catch(FileNotFoundException e){
             System.out.println("Arquivo não encontrado");
         }
-        this.montaHashs();
         this.montaChave(texto.length());
         System.out.println("Chave: " + chave);
         this.textoCifrado = this.cifrar(texto.toString(), this.chave);
@@ -64,12 +61,6 @@ public class vernam {
         Random random = new Random();
         for (int i = 0; i < tamanho; i++) {
             this.chave += alfabeto[random.nextInt(61)];
-        }
-    }
-    public void montaHashs(){
-        for(int i = 0; i < this.alfabeto.length; i++){
-            charPosicao.put(this.alfabeto[i], i);
-            posicaoChar.put(i, this.alfabeto[i]);
         }
     }
 
